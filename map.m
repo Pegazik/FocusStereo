5568d40caed7ef8d1ec13c445283aa648414708e
Pegazik <kuba.pegza@gmail.com>
2017-05-17 13:39:17 +0200

    Add files via upload
    Funkcja mapująca, zaimplementowane pary name-value i sprawdzanie atrybutów.

--- map.m
+++ map.m
@@ -1,9 +1,14 @@
 function[mapa] = map(ptCloud, varargin)
-%ptCloud - point cloud, filtered
-%arg 2: box - size of single map pixel in real life (in centimeters)
-%arg 3: [x1, x2] - upper and lower limits of x axis(width)
-%arg 4: [z1, z2] - upper and lower limits of z axis(depth)
-%arg 5: [y1, y2] - upper and lower limits of y axis(height)
+%ptCloud - point cloud
+%Name-value pairs:
+%box - size of single map pixel in real life (in centimeters)
+%default 1
+%xlim - [x1, x2] - upper and lower limits of x axis(width) 
+%default [-5000, 5000]
+%zlim - [z1, z2] - upper and lower limits of z axis(depth)
+%default [-5000, 5000]
+%ylim - [y1, y2] - upper and lower limits of y axis(height)
+%default [-2000, 4000]
 %output: map of size (x2-x1, y2-y1)/10*box in RGB, where: 
 %R represents medium height in milimeters mapped to 0-255
 %G represents weight (confidence) of measurement, being no. of measurements
@@ -11,45 +16,58 @@ function[mapa] = map(ptCloud, varargin)
 %B - not implemented yet. Will represent in 0-1 if terrain is traversible
 %or traversibility of terrain defined by smallest or median directional
 %derivative of height
-if(nargin<5)
-    ythresh = 4000;
-    dythresh = -2000;
-else
-    ythresh = varargin{4}(1);
-    dythresh = varargin{4}(2);
-end;
-if(nargin<4)
-    zthresh = 5000;
-    dzthresh = -5000;
-else
-    zthresh = varargin{3}(1);
-    dzthresh = varargin{3}(2);
-end;
-if(nargin<3)
-    xthresh = 5000;
-    dxthresh = -5000;
-else
-    xthresh = varargin{2}(1);
-    dxthresh = varargin{2}(2);
-end;
-if(nargin<2)
-    box = 1;
-else
-    box = varargin{1};
-end
-sx = abs(xthresh-dxThresh);
-sy = abs(ythresh-dyThresh);
-sz = abs(zthresh-dzThresh);
+p = inputParser;
 
-mapa = zeros(abs(zthresh-dzThresh)/(10*box), abs(xthresh-dxThresh)/(10*box), 3);
+defaultxlim = [-5000, 5000]; 
+defaultzlim = [-5000, 5000];
+defaultylim = [-2000, 4000];
+defaultbox = 1;
+
+classes = {'numeric'};
+attributes = {'size',[1, 2]};
+validFcn = @(f) validateattributes(f, classes, attributes);
+
+addParamValue(p,'xlim',defaultxlim,validFcn);
+addParamValue(p,'ylim',defaultylim,validFcn);
+addParamValue(p,'zlim',defaultzlim,validFcn);
+addParamValue(p, 'box', defaultbox, @isnumeric);
+parse(p,varargin{:});
+
+dxthresh= p.Results.xlim(1);
+xthresh= p.Results.xlim(2);
+dythresh = p.Results.ylim(1);
+ythresh = p.Results.ylim(2);
+dzthresh = p.Results.zlim(1);
+zthresh = p.Results.zlim(2);
+box = p.Results.box;
+
+sx = abs(xthresh-dxthresh);
+sy = abs(ythresh-dythresh);
+sz = abs(zthresh-dzthresh);
+
+points3D = ptCloud.Location;
+color = ptCloud.Color;
+k = 0;
+for i = 1:size(points3D, 1)
+    if(points3D(i, 1) > xthresh || points3D(i, 1) < dxthresh || points3D(i, 2) > ythresh ||...
+           points3D(i, 2) < dythresh || points3D(i, 3) > zthresh || points3D(i, 3) < dzthresh)
+        k = k+1;
+        del(k) = i;
+    end
+end
+
+points3D(del, :) = [];
+color(del, :) = [];
+
+mapa = zeros(abs(zthresh-dzthresh)/(10*box), abs(xthresh-dxthresh)/(10*box), 3);
 mapa(:,:,3) = 1;
 for i = 1:size(points3D, 1)
     x = points3D(i, 1);
     y = points3D(i, 2);
     z = points3D(i, 3);
-    px = floor( (x - dxThresh)/(sx)*size(mapa, 1))+1;
-    py = floor( (z - dzThresh)/(sz)*size(mapa, 2))+1;
-    mapa(px, py, 1) = (((y - dyThresh)/(sy)*255)/(mapa(px, py, 2)+1) + mapa(px, py, 1)*mapa(px, py, 2)/(mapa(px, py, 2)+1))/255;
+    px = floor( (x - dxthresh)/(sx)*size(mapa, 1))+1;
+    py = floor( (z - dzthresh)/(sz)*size(mapa, 2))+1;
+    mapa(px, py, 1) = (((y - dythresh)/(sy)*255)/(mapa(px, py, 2)+1) + mapa(px, py, 1)*mapa(px, py, 2)/(mapa(px, py, 2)+1))/255;
     mapa(px, py, 2) = mapa(px, py, 2)+1/255;
 end
 mapa = permute(mapa, [2 1 3]);
